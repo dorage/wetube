822027763cb64d789ab8652157b2023ef4f4ec8d
dorage <baloonflower554@gmail.com>
2019-02-20 20:36:34 +0900

    ~#2.16

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 
+app.use(helmet());
 app.set('view engine', "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+// 로컬 변수를 글로벌 변수로 만들어줌
+app.use(localsMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.home, userRouter);
