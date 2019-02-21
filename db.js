604771a312aada70564e1c110072f6c7c791d5f4
dorage <baloonflower554@gmail.com>
2019-02-21 19:43:55 +0900

    ~#2.21

---
+++ db.js
@@ -0,0 +1,42 @@
+export const videos = 
+[
+    {
+        id:3249365,
+        title: 'Video awesome',
+        description: 'This is awesome thing',
+        views:24,
+        videoFile:"https://archive.org/details/BugBuckBunny_124",
+        creator:
+        {
+            id: 121212,
+            name:"Dorage",
+            email:"dorage@dorage.com"
+        }
+    },
+    {
+        id:1412,
+        title: 'Video asdfg',
+        description: 'This is awesome thing',
+        views:24,
+        videoFile:"https://archive.org/details/BugBuckBunny_124",
+        creator:
+        {
+            id: 121212,
+            name:"Dorage",
+            email:"dorage@dorage.com"
+        }
+    },
+    {
+        id:124125215,
+        title: 'Video qwerty',
+        description: 'This is awesome thing',
+        views:24,
+        videoFile:"https://archive.org/details/BugBuckBunny_124",
+        creator:
+        {
+            id: 121212,
+            name:"Dorage",
+            email:"dorage@dorage.com"
+        }
+    }
+]
