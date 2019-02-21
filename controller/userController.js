7c6b91c1e5056a58b595eaf747602c204c8020d4
dorage <baloonflower554@gmail.com>
2019-02-21 20:50:21 +0900

    ~#2.24

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,6 @@ export const getJoin = (req, res) =>
 {
     res.render("join", {pageTitle : "Join"});
 };
-
 export const postJoin = (req, res) =>
 {
     // ES6 방식
@@ -27,7 +26,14 @@ export const postJoin = (req, res) =>
     res.render("join", {pageTitle : "Join"});
 };
 
-export const login = (req, res) => res.render("login", {pageTitle : "Login"});
+export const getLogin = (req, res) =>
+{
+    res.render("login", {pageTitle : "Login"});
+};
+export const postLogin = (req, res) =>
+{
+    res.redirect(routes.home);
+};
 export const logout = (req, res) => res.render("logout", {pageTitle : "Logout"});
 export const userDetail = (req, res) => res.render("userDetail", {pageTitle : "User Detail"});
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle : "Edit Profile"});
