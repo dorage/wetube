80fbef1a8f44803eaf16ce68022547d1992084e6
dorage <baloonflower554@gmail.com>
2019-02-24 20:11:48 +0900

    ~#3.6

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,7 @@
+import multer from "multer"
 import routes from "./routes";
+
+const multerVideo = multer({dest : "videos/"});
 
 export const localsMiddleware = (req, res, next) =>
 {
@@ -11,3 +14,5 @@ export const localsMiddleware = (req, res, next) =>
     }
     next();
 };
+
+export const uploadVideo = multerVideo.single('videoFile');
