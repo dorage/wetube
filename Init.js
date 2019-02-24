29e471e7db839252322e6b53535c21be50517aab
dorage <baloonflower554@gmail.com>
2019-02-24 19:03:55 +0900

    ~#3.2

--- Init.js
+++ Init.js
@@ -1,7 +1,9 @@
-import app from "./app";
 import "./db"
+import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4001;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
