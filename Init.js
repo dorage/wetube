d2d25acc052a9bef18534eb8735302fad75dfe89
dorage <baloonflower554@gmail.com>
2019-03-04 19:50:57 +0900

    ~#6.1

--- Init.js
+++ Init.js
@@ -2,6 +2,10 @@ import './db';
 import dotenv from 'dotenv';
 import app from './app';
 
+import './models/Video';
+import './models/Comment';
+import './models/User';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
